167bf540995fcafc02e5930c3e2dbc526128a15a
FaridMaIikov <malikow.farid@gmail.com>
2023-12-18 14:41:31 +0400

    tasks added

---
+++ src/main/java/com/example/example10/ParkRanger.java
@@ -0,0 +1,10 @@
+package com.example.example10;
+
+public class ParkRanger {
+
+    public static void main(String[] data) {
+        int birds = 10;
+        int trees = 5;
+        System.out.print(trees+birds );
+    }
+}
